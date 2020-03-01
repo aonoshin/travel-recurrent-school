b94dc39b7e59c9d674c677d48a37d903734b887e
EC2 Default User <ec2-user@ip-172-31-40-138.ap-northeast-1.compute.internal>
2020-03-01 11:29:06 +0000

    d

--- db/migrate/20200216054333_add_release_to_informations.rb
+++ db/migrate/20200216054333_add_release_to_informations.rb
@@ -1,5 +1,5 @@
 class AddReleaseToInformations < ActiveRecord::Migration[5.0]
   def change
-    add_column :informations, :release, :string
+    # add_column :informations, :release, :string
   end
 end
